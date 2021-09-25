b10c219fbf87974d6e4c662c88878eda5de4a75e
Fernando <fdo.clarosmith@gmail.com>
2021-09-25 00:21:01 -0300

    Removed python file

--- main.py
+++
@@ -1,8 +0,0 @@
-from boto.s3.connection import S3Connection
-import subprocess
-import os
-
-
-s3 = S3Connection(os.environ['DISCORD_TOKEN'])
-print(f"Debug: Discord key: {os.environ['DISCORD_TOKEN']}")
-subprocess.run(f"go run discordtyping.go -t {os.environ['DISCORD_TOKEN']}")
